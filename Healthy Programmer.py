720c0279fffe0741431baad6819a0ceb154d6f41
sandy-ji-code <sandeepypb@gmail.com>
2020-02-08 07:25:19 +0530

    modify wait() function

--- Healthy Programmer.py
+++ Healthy Programmer.py
@@ -13,15 +13,19 @@ def speak(str):
     speak.Speak(str)
 
 
+def wait():  # it's waiting for your pressing enter.
+    speak("okay, no problem !")
+    wait = input()
+
+
 def __my_rutein__():
     # set the timer according your activity and do your work time to time
     speak("Hey, i'm here for you, don't worry")
     permission = input()  # it take permission to enter the loop
-    if(permission == "yeah"):
+    if permission == "yeah":
         pass
-    else:   # if you type any other input like "wait" then the program hass been stop, amd waiting while you press enter.
-        speak("okay, no problem !")
-        wait = input()
+    else:  # if you type any other input like "wait" then the program hass been stop, go to wait()
+        wait()
     while True:
         print("...")  # its sign for loop start again
         for _ in range(6):
@@ -29,20 +33,19 @@ def __my_rutein__():
             time.sleep(600)
             speak("plz take some water ")
             water = input()
-            if (water == "wait"):  # if you busy tell them "wait"
-                speak("yeah")
-                wait = input()  # again press enter to start
-            elif (water == "done"):
+            if water == "wait":  # if you busy tell them "wait"
+                wait()  # again press enter to start
+            elif water == "done":
                 speak(" Well Done ")
-            elif (water == "final"):  # you want to stop the program, just type "final"
+            elif water == "final":  # you want to stop the program, just type "final"
                 speak(" Thank you !")
                 exit()
         # after 1 hr program tell you that you need some rest for your eyes ...
         speak("sir, i think you need a break, plz relex your Eyes, & do some Exercise.")
         eyes_exercise = input()  # after the break you start this program again just type "done"
-        if (eyes_exercise == "done"):
+        if eyes_exercise == "done":
             speak("Congratulation")
-        elif (eyes_exercise == "final"):  # you want to stop the program, just type "final"
+        elif eyes_exercise == "final":  # you want to stop the program, just type "final"
             speak(" Thank you !")
             exit()
 
